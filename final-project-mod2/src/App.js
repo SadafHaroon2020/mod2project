7fed0b84807b7f0bc67a7337f07c2a21e5a94847
SadafHaroon2020 <sadafssuet05@gmail.com>
2020-08-17 00:56:10 -0500

    fixing error

--- final-project-mod2/src/App.js
+++ final-project-mod2/src/App.js
@@ -19,8 +19,9 @@ function App() {
               <Route path='/about' component={About} />
               <Route path='/contact' component={Contact} />
             </Switch>
-            </Router>
             </div>
+            </Router>
+            
     </div>
   );
 }
