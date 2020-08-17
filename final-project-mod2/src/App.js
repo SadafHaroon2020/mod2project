79f4971c7fa1f41673550988ae7447dc6019a192
SadafHaroon2020 <sadafssuet05@gmail.com>
2020-08-17 00:45:59 -0500

    Making Nav bar using Router-Switch

--- final-project-mod2/src/App.js
+++ final-project-mod2/src/App.js
@@ -5,7 +5,22 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    
+    <div className="nav-bar">
+      <Router>
+          <div className='nav-route'>
+            <nav>
+              <ul className='list'>
+                <li><Link to='/about'>About</Link></li>
+                <li><Link to='/contact'>Contact</Link></li>
+              </ul>
+            </nav>
+
+            <Switch>
+              <Route path='/about' component={About} />
+              <Route path='/contact' component={Contact} />
+            </Switch>
+            </Router>
+            </div>
     </div>
   );
 }
