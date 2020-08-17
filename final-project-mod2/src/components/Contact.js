64895cd4b2455d22033f165e95130ea867cf2032
SadafHaroon2020 <sadafssuet05@gmail.com>
2020-08-17 00:51:52 -0500

    working

--- final-project-mod2/src/components/Contact.js
+++ final-project-mod2/src/components/Contact.js
@@ -1,11 +1,12 @@
 import React from 'react';
 class Contact extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
+    
     render() { 
-        return (  );
+        return (  
+            <div>
+
+            </div>
+        );
     }
 }
  
