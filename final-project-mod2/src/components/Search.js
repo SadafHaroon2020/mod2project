b5c65c42df329af3a4aaba78e3bdb7fd750dbe12
SadafHaroon2020 <sadafssuet05@gmail.com>
2020-08-18 12:16:17 -0500

    removing logs

--- final-project-mod2/src/components/Search.js
+++ final-project-mod2/src/components/Search.js
@@ -1,7 +1,6 @@
 import React, {useState} from 'react'
 
 const Search = (props) => {
-  console.log(props)
   const [searchValue, setSearchValue] = useState("");
 
   const handleSearchInputChanges = (e) => {
