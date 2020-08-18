dfdfb9dc6062fd81be075c2b23e31ec806861b55
SadafHaroon2020 <sadafssuet05@gmail.com>
2020-08-17 23:36:16 -0500

    working on components

--- final-project-mod2/src/components/Search.js
+++ final-project-mod2/src/components/Search.js
@@ -1,6 +1,35 @@
-import React from 'react';
-const [searchValue, setSearchValue] = useState("");
+import React, {useState} from 'react'
 
-const handleSearchInputChanges = (e) => {
-  setSearchValue(e.target.value);
+const Search = (props) => {
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleSearchInputChanges = (e) => {
+    setSearchValue(e.target.value);
+  }
+
+  const resetInputField = () => {
+    setSearchValue("");
+  }
+
+  const callSearchFunction = (e) => {
+    e.preventDefault();
+    props.search(searchValue);
+    resetInputField();
+  }
+
+  return (
+    <div>
+      <form className="search">
+        <input
+          value={searchValue}
+          onChange={handleSearchInputChanges}
+          type="text"
+          placeholder="Search..."
+        />
+        <input onClick={callSearchFunction} type="submit" value="SEARCH" />
+      </form>
+    </div>
+  )
 }
+
+export default Search
