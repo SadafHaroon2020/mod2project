adc0074592858eb7ef8ddef8b80e08566e6ae642
SadafHaroon2020 <sadafssuet05@gmail.com>
2020-08-17 23:43:38 -0500

    working on data rendering looks

--- final-project-mod2/src/components/Movie.js
+++ final-project-mod2/src/components/Movie.js
@@ -1,1 +1,6 @@
 const DEFAULT_PLACEHOLDER_IMAGE = "https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg";
+//making a function to show movie details
+function showMovieDetails(movieTitle, movieYear) {
+    let movieDetails = `Movie '${movieTitle}' was release in year ${movieYear}.`;
+    alert(movieDetails)
+  }
