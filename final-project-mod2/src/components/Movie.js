f675c61ded57cbabcb24585027451f4ac279a720
SadafHaroon2020 <sadafssuet05@gmail.com>
2020-08-19 00:47:30 -0500

    Updates: 2020-08-19 00:47:30

--- final-project-mod2/src/components/Movie.js
+++ final-project-mod2/src/components/Movie.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const DEFAULT_PLACEHOLDER_IMAGE = "https://lh3.googleusercontent.com/proxy/jhdKyQ2YNNqhqa9Cfcr2on4_xeTJyPbYp_TavsDrMB93BtRvGnSDkQE5ngiuD6MHpzMHHBro51bhw4E7cpLL6yLaTQ1nOD-g";
 
-function showMovieDetails(movieTitle, movieYear) {
+const showMovieDetails=(movieTitle, movieYear)=> {
   let movieDetails = `Movie '${movieTitle}' was release in year ${movieYear}.`;
   alert("MOVIE DETAILS: " + "\n"
     + "\n"
