874926e2675bf8eed12d2b479e7d96b6dcb780e1
SadafHaroon2020 <sadafssuet05@gmail.com>
2020-08-18 23:46:06 -0500

    code updates

--- final-project-mod2/src/components/Movie.js
+++ final-project-mod2/src/components/Movie.js
@@ -1,15 +1,17 @@
 //  import React, {useState} from 'react'
 import React from 'react'
 
-const DEFAULT_PLACEHOLDER_IMAGE = "https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg";
+const DEFAULT_PLACEHOLDER_IMAGE = "https://lh3.googleusercontent.com/proxy/jhdKyQ2YNNqhqa9Cfcr2on4_xeTJyPbYp_TavsDrMB93BtRvGnSDkQE5ngiuD6MHpzMHHBro51bhw4E7cpLL6yLaTQ1nOD-g";
 
 function showMovieDetails(movieTitle, movieYear) {
   let movieDetails = `Movie '${movieTitle}' was release in year ${movieYear}.`;
-  alert(movieDetails)
+  alert("MOVIE DETAILS: " + "\n"
+    + "\n"
+    + movieDetails);
 }
 
-const  Movie = (props) => {
-  let {movie, details} = props;
+const Movie = (props) => {
+  let { movie, details } = props;
   const poster = movie.Poster === 'N/A' ? DEFAULT_PLACEHOLDER_IMAGE : movie.Poster;
   return (
     <div className="movie" onClick={() => showMovieDetails(movie.Title, movie.Year)}>
