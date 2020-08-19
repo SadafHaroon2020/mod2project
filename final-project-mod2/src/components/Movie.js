b5bfbe448945e49e41d2d4f55c9b6f1708be934b
SadafHaroon2020 <sadafssuet05@gmail.com>
2020-08-18 23:54:40 -0500

    Updated: 2020-08-18 23:54:40

--- final-project-mod2/src/components/Movie.js
+++ final-project-mod2/src/components/Movie.js
@@ -1,4 +1,3 @@
-//  import React, {useState} from 'react'
 import React from 'react'
 
 const DEFAULT_PLACEHOLDER_IMAGE = "https://lh3.googleusercontent.com/proxy/jhdKyQ2YNNqhqa9Cfcr2on4_xeTJyPbYp_TavsDrMB93BtRvGnSDkQE5ngiuD6MHpzMHHBro51bhw4E7cpLL6yLaTQ1nOD-g";
